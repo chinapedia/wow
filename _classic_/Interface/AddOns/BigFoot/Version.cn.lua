5bf7fd323b31574f6168e36279a5b4800027e390
zhangban <1847282863@qq.com>
2019-10-11 08:38:04 +0800

    Interface.1.13.2.19.zip

--- _classic_/Interface/AddOns/BigFoot/Version.cn.lua
+++ _classic_/Interface/AddOns/BigFoot/Version.cn.lua
@@ -1,6 +1,6 @@
 if GetLocale()~='zhCN' then return end
 local main= "1.13.2."
-local minor = "18"
+local minor = "19"
 BIGFOOT_VERSION = "zhCN"..main..minor;
 
 function GetMinorVersion()
@@ -8,6 +8,12 @@ function GetMinorVersion()
 end
 
 BigFootChangelog_ah();
+
+	BigFootChangelog_at("2019/10/08 (1.13.2.19)")
+	BigFootChangelog_ar("目标信息","现在可以单独显示额外百分比提示。")
+	BigFootChangelog_ar("玩家链接","修复一个已知问题。")
+	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
+
 
 	BigFootChangelog_at("2019/09/29 (1.13.2.18)")
 	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
@@ -114,4 +120,4 @@ BigFootChangelog_ah();
 BigFootChangelog_af();
 
 
-BF_VERSION_CHECKSUM = "57"
+BF_VERSION_CHECKSUM = "56"
